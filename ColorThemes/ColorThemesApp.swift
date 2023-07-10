471783c9c95fd8fcd920f1f0cdfb80c2fd578a1e
Parker Ehret <parkerehret@users.noreply.github.com>
2023-07-10 10:02:24 -0700

    Initial Commit

---
+++ ColorThemes/ColorThemesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorThemesApp.swift
+//  ColorThemes
+//
+//  Created by Parker Ehret on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorThemesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
