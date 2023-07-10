73d5e0c0e16af60be560f9175895f791e4dbba30
Parker Ehret <parkerehret@users.noreply.github.com>
2023-07-10 10:47:08 -0700

    Updated Events and opacity

--- ColorThemes/ContentView.swift
+++ ColorThemes/ContentView.swift
@@ -12,26 +12,166 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
+            HStack {
+                ZStack {
+                    Rectangle()
+                        .fill(themeManager.currentTheme.color1.opacity(1.0))
+                        .frame(width: 44, height: 44)
+                        .cornerRadius(18)
+                    
+                    Text("13")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                }
+                .padding(.leading, 16)
+                .padding(.vertical, 16)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Birthday")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(themeManager.currentTheme.color1.opacity(1.0))
+                    }
+                    HStack {
+                        Text("This event will happen this year")
+                            .font(.footnote)
+                            .fontWeight(.medium)
+                            .foregroundColor(themeManager.currentTheme.color1.opacity(0.5))
+                    }
+                    .padding(.top, -8)
+                }
+                .padding(.leading, 8)
+                Spacer()
+            }
+            .background(themeManager.currentTheme.color1.opacity(0.05))
+            .cornerRadius(28)
+            .padding(.horizontal, 16)
+            
+            HStack {
+                ZStack {
+                    Rectangle()
+                        .fill(themeManager.currentTheme.color2.opacity(1.0))
+                        .frame(width: 44, height: 44)
+                        .cornerRadius(18)
+                    
+                    Text("13")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                }
+                .padding(.leading, 16)
+                .padding(.vertical, 16)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Holiday")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(themeManager.currentTheme.color2.opacity(1.0))
+                    }
+                    HStack {
+                        Text("This event will happen this year")
+                            .font(.footnote)
+                            .fontWeight(.medium)
+                            .foregroundColor(themeManager.currentTheme.color2.opacity(0.5))
+                    }
+                    .padding(.top, -8)
+                }
+                .padding(.leading, 8)
+                Spacer()
+            }
+            .background(themeManager.currentTheme.color2.opacity(0.05))
+            .cornerRadius(28)
+            .padding(.horizontal, 16)
+            
+            HStack {
+                ZStack {
+                    Rectangle()
+                        .fill(themeManager.currentTheme.color3.opacity(1.0))
+                        .frame(width: 44, height: 44)
+                        .cornerRadius(18)
+                    
+                    Text("13")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                }
+                .padding(.leading, 16)
+                .padding(.vertical, 16)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Anniversary")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(themeManager.currentTheme.color3.opacity(1.0))
+                    }
+                    HStack {
+                        Text("This event will happen this year")
+                            .font(.footnote)
+                            .fontWeight(.medium)
+                            .foregroundColor(themeManager.currentTheme.color3.opacity(0.5))
+                    }
+                    .padding(.top, -8)
+                }
+                .padding(.leading, 8)
+                Spacer()
+            }
+            .background(themeManager.currentTheme.color3.opacity(0.05))
+            .cornerRadius(28)
+            .padding(.horizontal, 16)
+            
+            HStack {
+                ZStack {
+                    Rectangle()
+                        .fill(themeManager.currentTheme.color4.opacity(1.0))
+                        .frame(width: 44, height: 44)
+                        .cornerRadius(18)
+                    
+                    Text("13")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                }
+                .padding(.leading, 16)
+                .padding(.vertical, 16)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Custom Event")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(themeManager.currentTheme.color4.opacity(1.0))
+                    }
+                    HStack {
+                        Text("This event will happen this year")
+                            .font(.footnote)
+                            .fontWeight(.medium)
+                            .foregroundColor(themeManager.currentTheme.color4.opacity(0.5))
+                    }
+                    .padding(.top, -8)
+                }
+                .padding(.leading, 8)
+                Spacer()
+            }
+            .background(themeManager.currentTheme.color4.opacity(0.05))
+            .cornerRadius(28)
+            .padding(.horizontal, 16)
+
+            
             Rectangle().fill(themeManager.currentTheme.color1)
                 .cornerRadius(18)
                 .padding(.horizontal, 16)
-                .padding(.bottom, 16)
+                .padding(.top, 16)
                 .frame(height: 76)
             Rectangle().fill(themeManager.currentTheme.color2)
                 .cornerRadius(18)
                 .padding(.horizontal, 16)
                 .padding(.bottom, 16)
                 .frame(height: 76)
-            Rectangle().fill(themeManager.currentTheme.color3)
-                .cornerRadius(18)
-                .padding(.horizontal, 16)
-                .padding(.bottom, 16)
-                .frame(height: 76)
-            Rectangle().fill(themeManager.currentTheme.color4)
-                .cornerRadius(18)
-                .padding(.horizontal, 16)
-                .padding(.bottom, 16)
-                .frame(height: 76)
+
             ForEach(ColorThemeName.allCases, id: \.self) { theme in
                 Button(theme.rawValue) {
                     themeManager.changeTheme(theme: theme)
